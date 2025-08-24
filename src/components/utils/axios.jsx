45ac9a2cbbee7a71b8a725022240ff6223ebb2c0
abhishekbain220 <you@example.co>
2025-08-24 20:37:30 +0530

    2

--- src/components/utils/axios.jsx
+++ src/components/utils/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let instance=axios.create({
     baseURL:"https://onlinetestbackend-1a0p.onrender.com",
-    withCredentials:true
+    withCredentials:
 })
 
 export default instance
